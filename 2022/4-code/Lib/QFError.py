b57bdc499207ab38b9c6a422794d2f2e4f33399e
Giovanni Della Lunga <polyhedron-gdl@users.noreply.github.com>
2022-05-13 13:16:02 +0200

    dev

--- 2022/4-code/Lib/QFError.py
+++
@@ -1,8 +0,0 @@
-class QFError(Exception):
-    "A custom exception used to report QF students errors "
-
-    def __init__(self, text):
-        self.text = text
-
-    def showErr(self):
-        print("@ %-12s: %s" %("Error", self.text))
